9610368b8136f4fe6233317409c17cd65bd2a940
Your Name <dagimwondim25@gmail.com>
2023-10-08 23:31:36 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+import Main from './Component/Main';
+
+import aaa from './s-class.webp'
+import bbb from './hh.webp'
+import ddd from './ss.webp'
+import fff from './ee.jpeg'
+import ggg from './sc.webp'
+import hhh from './dd.webp'
+import iii from './rr.webp'
+import jjj from './gg.webp'
+
+import kkk from './ff.webp'
+import lll from './sss.webp'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<Header />
+    <Main 
+    pri='$200'
+    bra='Mercedes'
+    mod='S Class Limousine'
+    loc='Addis Ababa'
+    aa={aaa}
+    bb={bbb}
+    dd={ddd}
+    ff={fff}
+    gg={ggg}
+    hh={hhh}
+    ii={iii}
+    jj={jjj}
+    kk={kkk}
+    ll={lll}
+     />
+    <Footer />
     </div>
   );
 }
 
+
 export default App;
+
+
+
+
+ 
---
+++ src/Component/JJJ.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default function JJJ(){
+    return (
+        <h1>This is Home page</h1>
+    )
+}
